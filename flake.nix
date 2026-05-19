520e859a66c92b41db81f1c99525879ddedadd06
phyrophone <255566614+phyrophone@users.noreply.github.com>
2026-05-19 16:41:35 +0200

    update flake to unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A Linux Task Manager alternative built with Qt6, inspired by the Windows Task Manager but designed to go further - providing deep visibility into system processes, performance metrics, users, and services.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
